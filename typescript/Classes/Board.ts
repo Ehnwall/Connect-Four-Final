38c978411e1aaec46ed2d0ffab7c9eb7a843f59b
Ehnwall <dennis.ehnwall@gmail.com>
2024-09-15 19:03:58 +0200

    add print to board

--- typescript/Classes/Board.ts
+++ typescript/Classes/Board.ts
@@ -6,4 +6,16 @@ export default class Board {
     this.matrix = [...new Array(6)].map(() => [...new Array(7)].map(() => null));
     this.gameOver = false;
   }
+  print() {
+    const horizontalSeparator = '+---'.repeat(7) + '+';
+    const boardString = this.matrix
+      .map((row) => {
+        const rowString = row.map((cell) => ` ${cell === null ? ' ' : cell} `).join('|');
+        return `${horizontalSeparator}\n|${rowString}|`;
+      })
+      .join('\n');
+    console.log(boardString);
+    console.log(horizontalSeparator);
+    console.log('  1   2   3   4   5   6   7');
+  }
 }
