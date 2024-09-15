a7e795d2e25c429e1cfd5541ab2a21033efd4ef0
Ehnwall <dennis.ehnwall@gmail.com>
2024-09-15 18:58:18 +0200

    removed comment

--- typescript/Classes/Player.ts
+++ typescript/Classes/Player.ts
@@ -1,7 +1,6 @@
 export default class Player {
   name: string;
-  marker: 'X' | 'O'; // Correctly define marker type
-
+  marker: 'X' | 'O';
   constructor(name: string, marker: 'X' | 'O') {
     this.name = name;
     this.marker = marker;
